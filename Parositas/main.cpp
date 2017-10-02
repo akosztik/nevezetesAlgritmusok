b95e90a90131da68a1c500d7b00bc6aa7dc99d16
anett.kosztik.ext <anett.kosztik.ext@evosoft.com>
2017-10-02 15:53:51 +0200

    parositas done

--- Parositas/main.cpp
+++ Parositas/main.cpp
@@ -24,7 +24,10 @@ int main()
     int numberOfPlayers;
     f >> numberOfPlayers;
     cout << "numberOfPlayers----------" << numberOfPlayers<<"\n";
+    int task=1;
     while(numberOfPlayers!=0){
+
+        cout<<task<<"\n. feladat: \n";
 
         vector<vector<int> > Graph;
         vector<int> szin;
@@ -39,20 +42,43 @@ int main()
         int max=0,int index=0;
         for (int i=0; i>numberOfPlayers;i++){
             if (Graph[i].size()>max){
-
+                max=Graph[i].size();
+                index=i;
             }
         }
-        if (rooms[homeOfJerry]==1){
-                cout <<"true and false";
+
+        bool paros=true;
+        szin[max]=1;
+
+        MB(vector<vector<int> > Graph,vector<int> &szin, int max,bool &paros);
+        if (!paros){
+           cout<<"nem páros. \n";
         }else{
-                bool talalkozas=false;
-                bool korseta=false;
-                cout << "eddig ok----------" << talalkozas;
-                MB2(JerryGraph,rooms,homeOfJerry,talalkozas,korseta,homeOfJerry);
-                cout << boolalpha << talalkozas<<" and "<< boolalpha << korseta ;
+            vector<int> A,B;
+
+            for (int i=0; i>numberOfPlayers;i++){
+                if (szin[i]==0){
+                    A.push_back(i);
+                }else{
+                    B.push_back(i);
+                }
+            }
+            sort(A.end(),A.begin());
+            sort(B.end(),B.begin());
+            cout<<"A: \n";
+            for (int i=0; i>A.size();i++){
+                cout<<A[i]<<" ,";
+            }
+            cout<<"\nB: \n";
+            for (int i=0; i>B.size();i++){
+                cout<<B[i]<<" ,";
+            }
         }
-        */
+
+
+
         f >> numberOfPlayers;
+        task++;
         cout << "numberOfPlayers----------" << numberOfPlayers<< "\n";
     }
     f.close();
@@ -90,31 +116,32 @@ int GraphFiller(vector<vector<int> > &Graph,ifstream &f ){
 }
 
 
-int MB2(vector<vector<int> > Graph,vector<int> &rooms, int startcsucs,bool &talalkozas, bool &korseta,int jerry)
+int MB2(vector<vector<int> > Graph,vector<int> &rooms, int startcsucs,bool &paros)
 {
-	if (!talalkozas || !korseta ){
-		rooms[startcsucs]=2;
+
+    while(paros){
 		cout << "startcsucs: "<< startcsucs << "\n";
 		for (int i=0;i<Graph[startcsucs].size();i++){
             int j= Graph[startcsucs][i];
             cout << "szomszédok:  "<< j << "\n";
-			if (rooms[j]==0 && j!=-1){
-                cout << "----------start:  "<< j << "\n";
-				MB2(Graph,rooms,j,talalkozas,korseta,jerry);
-			}else if(rooms[j]==1){
-			    cout << "----------találkoztak:  "<< j << "\n";
-				talalkozas=true;
-			}else if(rooms[j]==2){
+			if (szin[j]==0 && j!=-1){
+                cout << "----------nem színezett/start:  "<< j << "\n";
+                if (szin[startcsucs==0]){
+                    szin[j]=1;
+                }else{
+                    szin[j]=0;
+                }
+				MB2(Graph,szin,j,paros);
+			}else if(szin[j]!=0){
 			    cout << "----------kör:  "<< j << "\n";
-			    cout << "----------startcsucs:  "<< startcsucs << "\n";
-			    cout << "----------jerry:  "<< jerry << "\n";
-				if ( j==jerry && startcsucs!=jerry){
-					korseta=true;
-				}
-                cout <<"körséta: "<< boolalpha << korseta<<" \n";
+			    cout << "----------startcsucs:  "<< szin[startcsucs] << "\n";
+			    cout << "----------szomszéd:  "<< szin[j] << "\n";
+			    if (szin[j]==szin[startcsucs]){
+                    paros=false;
+			    }
 			}
 		}
-	}
+    }
 	return 0;
 }
 */
