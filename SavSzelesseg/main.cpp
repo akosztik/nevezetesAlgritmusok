15e4659f0b86c76a8518858bd1eaacc87af0def9
Kosztik Anett <akosztik@gmail.com>
2017-10-09 19:18:16 +0200

    struct

--- SavSzelesseg/main.cpp
+++ SavSzelesseg/main.cpp
@@ -10,7 +10,17 @@
 using namespace std;
 
 
-int GraphFiller(vector<vector<pair <int,int> > > &Graph, ifstream &f , int kapcs);
+
+struct sav{
+        int to;
+        int suly;
+        sav(int x, int y){
+            this.to=x;
+            this.suly=y;
+        }
+};
+
+int GraphFiller(vector<vector<sav > > &Graph, ifstream &f , int kapcs);
 int MB(vector<vector<int> >& Graph,vector<int> &szin, int startcsucs,bool &paros);
 
 
@@ -25,22 +35,28 @@ int main()
     }
     int numberOfTasks;
     f >> numberOfTasks;
-    //cout << "numberOfPlayers----------" << numberOfPlayers<<"\n";
-    for (int=0;i<numberOfTasks;i++){
+
+    for (int i=0;i<numberOfTasks;i++){
 
         cout<<"\n**************"<<i+1<<". eset: \n";
 
-        vector<vector<pair <int,int> > > Graph;
+        vector<vector<sav> > Graph;
 
         //gépek és kapcsolatok számának beolvasása:
 
         int gepek, kapcs;
         f >> gepek >> kapcs;
 
+        vector<bool> kesz;
+
         for (int j=0;j<gepek;j++){
-            vector<int> x;
-            x.push_back(-1);
+            vector<sav> x;
+            sav s = new sav();
+            s.to=-1;
+            s.suly=-1;
+            x.push_back(s);
             Graph.push_back(x);
+            kesz.push_back(false);
         }
 
         GraphFiller(Graph, f, kapcs);
@@ -89,37 +105,46 @@ int main()
 
 
 
-int GraphFiller(vector<vector<pair <int,int> > > &Graph,ifstream &f , int kapcs){
+int GraphFiller(vector<vector<sav> > &Graph,ifstream &f , int kapcs){
     int from,to,suly;
+
     for (int i=0;i<kapcs;i++){
 
         f >> from >> to >> suly;
         cout << "from:  "<< from << ", to:  "<< to << ", suly:  "<< suly << "\n";
         from=from-1;
         to=to-1;
-        pair <int,int> sáv;
-        if (Graph[to][0]!=-1){
-            Graph[to].push_back(from);
-            cout << "from:  "<< from<< "\n";
+
+        if (Graph[to][0].to!=-1){
+            sav s=new sav();
+            sav.to=from;
+            sav=suly=suly;
+            Graph[to].push_back(sav);
+            cout << "from:  "<< to << ",to:  "<< from<< ",suly:  "<< suly  << "\n";
         }
-        if (Graph[from][0]!=-1){
-            Graph[from].push_back(to);
-            cout << "to:  "<< to<< "\n";
+        if (Graph[from][0].to!=-1){
+            sav s=new sav();
+            sav.to=to;
+            sav=suly=suly;
+            Graph[from].push_back(s);
+            cout << "from:  "<< from << ",to:  "<< to<< ",suly:  "<< suly  << "\n";
         }
-        if (Graph[from][0]==-1){
-            Graph[from][0]=to;
-            cout << "to:  "<< to<< "\n";
+        if (Graph[from][0].to==-1){
+            sav.to=to;
+            sav=suly=suly;
+            cout << "from:  "<< from << ",to:  "<< to<< ",suly:  "<< suly  << "\n";
 
         }
-        if (Graph[to][0]==-1){
-            Graph[to][0]=from;
-            cout << "from:  "<< from<< "\n";
+        if (Graph[to][0].to==-1){
+            sav.to=from;
+            sav=suly=suly;
+            cout << "from:  "<< to << ",to:  "<< from<< ",suly:  "<< suly  << "\n";
         }
     }
     return 0;
 }
 
-
+/*
 int MB(vector<vector<int> > &Graph,vector<int> &szin, int startcsucs,bool &paros)
 {
         //cout << "startcsucs: "<< startcsucs << "\n";
@@ -136,10 +161,10 @@ int MB(vector<vector<int> > &Graph,vector<int> &szin, int startcsucs,bool &paros
                     }
                     MB(Graph,szin,j,paros);
                 }else if( szin[j]==szin[startcsucs]){
-                    /*cout << "----------szinezett:  "<< j << "\n";
+                    cout << "----------szinezett:  "<< j << "\n";
                     cout << "----------startcsucs:  "<< szin[startcsucs] << "\n";
                     cout << "----------szomszéd:  "<< szin[j] << "\n";
-                    */
+
                     paros=false;
                 }
             }
@@ -150,3 +175,4 @@ int MB(vector<vector<int> > &Graph,vector<int> &szin, int startcsucs,bool &paros
 
 	return 0;
 }
+*/
