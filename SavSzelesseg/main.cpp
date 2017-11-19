40e30da4f784cfb637c6f63614806d74b079e005
akosztik <akosztik@gmail.com>
2017-11-20 00:14:05 +0100

    added savszel

--- SavSzelesseg/main.cpp
+++ SavSzelesseg/main.cpp
@@ -14,19 +14,15 @@ using namespace std;
 struct sav{
         int to;
         int suly;
-        sav(int x, int y){
-            this.to=x;
-            this.suly=y;
-        }
 };
 
 int GraphFiller(vector<vector<sav > > &Graph, ifstream &f , int kapcs);
-int MB(vector<vector<int> >& Graph,vector<int> &szin, int startcsucs,bool &paros);
+int FeltMaxKer(vector<vector<sav> >& Graph,vector<int> &d,vector<bool> &kesz, int startcsucs,int elozo, int& n);
 
 
 int main()
 {
-    ifstream f("net.in");
+    ifstream f("net4.in");
 
     if(f.fail()){
         cout << "Nincs meg a fájl!\n";
@@ -47,54 +43,59 @@ int main()
         int gepek, kapcs;
         f >> gepek >> kapcs;
 
+        vector<int> d;
         vector<bool> kesz;
 
         for (int j=0;j<gepek;j++){
             vector<sav> x;
-            sav s = new sav();
+            sav s;
             s.to=-1;
             s.suly=-1;
             x.push_back(s);
             Graph.push_back(x);
+            d.push_back(-1);
             kesz.push_back(false);
         }
 
         GraphFiller(Graph, f, kapcs);
-        /*
-        bool paros=true;
-        for (int i=0; i<szin.size();i++){
-                if (szin[i]==0 && paros){
-                    szin[i]=1;
-                    MB(Graph,szin,i,paros);
-                }
+
+        int n=0;
+        int start=0;
+
+        for (int z=0;z<Graph.size();z++){
+            if (Graph[z].size()==1 && Graph[z][0].to!=-1 ){
+                start=z;
+            }
         }
 
-        if (!paros){
-           cout<<"\n**************nem paros. \n";
+        FeltMaxKer(Graph, d,kesz, start,-1, n);
+
+        cout<<"************** \n";
+
+        int min=d[0];
+        bool osszefuggo=true;
+        for (int l=0; l<d.size();l++){
+            //cout<<"value, min: "<<d[l] <<"\n";
+            if (min==0){
+                min=d[l];
+            }
+            if (d[l]<min && d[l]!=0){
+                min=d[l];
+
+            }
+            if (min==-1){
+                osszefuggo=false;
+            }
+
+        }
+        if (osszefuggo){
+            cout<<"*****legkisebb d: ";
+            cout<<min <<"\n";
         }else{
-            vector<int> A,B;
-
-            for (int i=0; i<szin.size();i++){
-                if (szin[i]==1){
-                    A.push_back(i+1);
-
-                }else if(szin[i]==2){
-                    B.push_back(i+1);
-
-                }
-            }
-            cout<<"************** \n";
-            cout<<"*****A: ";
-            for (int i=0; i<A.size();i++){
-                   cout<<A[i]<<" ,";
-            }
-            cout<<"\n*****B: ";
-            for (int i=0; i<B.size();i++){
-                cout<<B[i]<<" ,";
-            }
-            cout<<"\n************** \n";
+            cout<<"*****nem osszefuggo: 0\n";
         }
-*/
+
+
     }
     f.close();
     return 0;
@@ -111,68 +112,69 @@ int GraphFiller(vector<vector<sav> > &Graph,ifstream &f , int kapcs){
     for (int i=0;i<kapcs;i++){
 
         f >> from >> to >> suly;
-        cout << "from:  "<< from << ", to:  "<< to << ", suly:  "<< suly << "\n";
+        //cout << "from:  "<< from << ", to:  "<< to << ", suly:  "<< suly << "\n";
         from=from-1;
         to=to-1;
 
         if (Graph[to][0].to!=-1){
-            sav s=new sav();
-            sav.to=from;
-            sav=suly=suly;
-            Graph[to].push_back(sav);
-            cout << "from:  "<< to << ",to:  "<< from<< ",suly:  "<< suly  << "\n";
+            sav s;
+            s.to=from;
+            s.suly=suly;
+            Graph[to].push_back(s);
+            //cout << "from:  "<< to << ",to:  "<< from<< ",suly:  "<< suly  << "\n";
         }
         if (Graph[from][0].to!=-1){
-            sav s=new sav();
-            sav.to=to;
-            sav=suly=suly;
+            sav s;
+            s.to=to;
+            s.suly=suly;
             Graph[from].push_back(s);
-            cout << "from:  "<< from << ",to:  "<< to<< ",suly:  "<< suly  << "\n";
+            //cout << "from:  "<< from << ",to:  "<< to<< ",suly:  "<< suly  << "\n";
         }
         if (Graph[from][0].to==-1){
-            sav.to=to;
-            sav=suly=suly;
-            cout << "from:  "<< from << ",to:  "<< to<< ",suly:  "<< suly  << "\n";
+            Graph[from][0].to=to;
+            Graph[from][0].suly=suly;
+            //cout << "from:  "<< from << ",to:  "<< to<< ",suly:  "<< suly  << "\n";
 
         }
         if (Graph[to][0].to==-1){
-            sav.to=from;
-            sav=suly=suly;
-            cout << "from:  "<< to << ",to:  "<< from<< ",suly:  "<< suly  << "\n";
+            Graph[to][0].to=from;
+            Graph[to][0].suly=suly;
+            //cout << "from:  "<< to << ",to:  "<< from<< ",suly:  "<< suly  << "\n";
         }
     }
     return 0;
 }
 
-/*
-int MB(vector<vector<int> > &Graph,vector<int> &szin, int startcsucs,bool &paros)
-{
-        //cout << "startcsucs: "<< startcsucs << "\n";
-        for (int i=0;i<Graph[startcsucs].size();i++){
-            int j= Graph[startcsucs][i];
-            //cout << "szomszédok:  "<< j << "\n";
-            if (j!=startcsucs && j!=-1){
-               if (  szin[j]==0 ){
-                    //cout << "----------nem színezett/start:  "<< j << "\n";
-                    if (szin[startcsucs]==1){
-                        szin[j]=2;
-                    }else{
-                        szin[j]=1;
-                    }
-                    MB(Graph,szin,j,paros);
-                }else if( szin[j]==szin[startcsucs]){
-                    cout << "----------szinezett:  "<< j << "\n";
-                    cout << "----------startcsucs:  "<< szin[startcsucs] << "\n";
-                    cout << "----------szomszéd:  "<< szin[j] << "\n";
 
-                    paros=false;
-                }
+int FeltMaxKer(vector<vector<sav> > &Graph,vector<int> &d,vector<bool> &kesz, int startcsucs,int elozo, int& n)
+{
+    //cout << "startcsucs:  "<< startcsucs << "***************************  \n";
+    kesz[startcsucs]=true;
+    if (Graph[startcsucs][0].to!=-1 && n<d.size()){
+        int max=-1;
+        int ind=0;
+        for (int i=0;i<Graph[startcsucs].size();i++){
+            if( Graph[startcsucs][i].suly > max && Graph[startcsucs][i].to!=elozo && !kesz[Graph[startcsucs][i].to]){
+                max=Graph[startcsucs][i].suly;
+                ind=i;
             }
 
 		}
+		if(n==d.size()-1){
+           d[startcsucs] =0;
+		}else{
+            d[startcsucs]=Graph[startcsucs][ind].suly;
+		}
 
+		n++;
+		//cout << "csucs:  "<< startcsucs << ", max sulya: "<< d[startcsucs] <<" , elozo: "<< elozo<< "\n";
+		if(Graph[startcsucs][ind].to!=elozo){
+            FeltMaxKer(Graph,d,kesz,Graph[startcsucs][ind].to,startcsucs,n);
+		}
 
-
+	}else{
+       //cout << "startcsucs:  "<< startcsucs << "nincsenek szomszedai vagy meg van minden csucs \n";
+	}
 	return 0;
 }
-*/
+
